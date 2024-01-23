5ebab0ff9e3b9a56e5e9906bfef5e4b5d5525eab
Cesar Cardenas <cecardev@gmail.com>
2024-01-22 20:23:11 -0600

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {};
+
+const HomePage = (props: Props) => {
+  return (
+    <>
+      <p>Home Page</p>
+      <div className="w-64 h-64 bg-red-500"></div>
+    </>
+  );
+};
+
+export default HomePage;
